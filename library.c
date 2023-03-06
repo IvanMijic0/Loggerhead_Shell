47e08b4cbc8840fb5dd8d4461e22d274c369eb99
ivan <ivanmijic159951@gmail.com>
2023-03-06 23:34:59 +0100

    Initial commit

---
+++ library.c
@@ -0,0 +1,14 @@
+#include "library.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/wait.h"
+#include "readline/readline.h"
+#include "readline/history.h"
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
+
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef LOGGERHEAD_SHELL_LIBRARY_H
+#define LOGGERHEAD_SHELL_LIBRARY_H
+
+void hello(void);
+
+#endif //LOGGERHEAD_SHELL_LIBRARY_H
